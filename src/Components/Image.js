f24ca1e54cfab23aee0d934c30bb6ffd4ef0ad07
rutrut6969 <rutledgeisaac6969@gmail.com>
2020-03-19 21:14:04 -0500

    Styling it up man! Created some style components not using library

--- src/Components/Image.js
+++ src/Components/Image.js
@@ -2,6 +2,50 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ImageTitle from './ImageTitle';
 import DataWrap from './ImageData';
+import styled from 'styled-components';
+const Day = styled.input`
+  border: none;
+  height: 20px;
+  width: 70px;
+  margin: 10px;
+  background: transparent;
+  color: rgba(211, 211, 211, 0.75);
+`;
+const Month = styled.input`
+  border: none;
+  height: 20px;
+  width: 75px;
+  margin: 10px;
+  background: transparent;
+  color: rgba(211, 211, 211, 0.75);
+`;
+const Year = styled.input`
+  border: none;
+  height: 20px;
+  width: 90px;
+  margin: 10px;
+  background: transparent;
+  color: rgba(211, 211, 211, 0.75);
+`;
+
+const Btn = styled.button`
+  border: none;
+  background-color: rgba(211, 211, 211, 0.75);
+  height: 30px;
+  width: 200px;
+  color: black;
+  border-radius: 5px;
+  margin: 10px;
+
+  &:hover {
+    cursor: pointer;
+    background-color: transparent;
+    border: 1px solid rgba(211, 211, 211, 0.75);
+    transition: 0.4s ease-in;
+    color: rgba(211, 211, 211, 0.75);
+  }
+`;
+
 function Image() {
   const [title, setTitle] = useState('');
   const [image, setImage] = useState('');
@@ -9,6 +53,8 @@ function Image() {
   const [desc, setDesc] = useState('');
   const [copy, setCopy] = useState('');
   const [hdUrl, setHdUrl] = useState('');
+  const [inVal, setInVal] = useState('');
+
   //   console.log(image);
   useEffect(() => {
     axios
@@ -26,15 +72,40 @@ function Image() {
       })
       .catch(err => console.log('Your error: ', err));
   }, []);
-  return (
-    <div className='wrapper'>
-      <ImageTitle title={title} date={date} />
-      <div className='imageWrap'>
-        <img src={image} alt={title} />
-      </div>
+  function click() {
+    setInVal(date.value);
+  }
+  const Wrap = styled.div`
+  background-image: url('${image}');
+  background-size: 100% 100%;
+  color: white;
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 
-      <DataWrap desc={desc} copy={copy} />
-    </div>
+`;
+  const HeadWrap = styled.section`
+    display: flex;
+    margin-top: -3%;
+    margin-bottom: 20%;
+  `;
+
+  return (
+    <Wrap className='wrapper'>
+      <HeadWrap className='date-wrapper'>
+        <Day className='day' placeholder='Day: 00' type='number' />
+        <Month className='month' placeholder='Month: 00' type='number' />
+        <Year className='year' placeholder='Year: 0000' type='number' />
+        <Btn onClick={click}>Set The Date Change The Pic!</Btn>
+      </HeadWrap>
+
+      <ImageTitle title={title} date={date} styled={styled} />
+      <div className='imageWrap'>{/* <img src={image} alt={title} /> */}</div>
+      <DataWrap desc={desc} copy={copy} styled={styled} />
+    </Wrap>
   );
 }
 
--- src/Components/ImageTitle.js
+++ src/Components/ImageTitle.js
@@ -1,7 +1,17 @@
 // Imports
 import React, { useState, useEffect } from 'react';
-
+import styled from 'styled-components';
 // Components
+
+const TitleWrap = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-content: center;
+  background: rgba(211, 211, 211, 0.75);
+  width: 85%;
+  border-radius: 10px;
+  color: black;
+`;
 function ImageTitle(props) {
   const title = props.title;
   const date = props.date;
@@ -16,10 +26,10 @@ function ImageTitle(props) {
   //   console.log(absDate);
 
   return (
-    <>
+    <TitleWrap>
       <p className='title'>{title},</p>
       <p className='date'> {absDate}</p>
-    </>
+    </TitleWrap>
   );
 }
 // Exports
